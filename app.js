ec56f75841326acbe0a9958b2b0fe661f4b2eac7
Daniela del Pino <danielaabigaildelpino@gmail.com>
2024-02-01 14:46:57 -0300

    Creacion Encriptador y Desencriptador de Texto

---
+++ app.js
@@ -0,0 +1,83 @@
+const ingresoTexto = document.querySelector(".texto");
+const botonEncriptar = document.querySelector(".btn-encriptar");
+const botonDesencriptar = document.querySelector(".btn-desencriptar");
+const mensajeFinal = document.querySelector("#textoFinal");
+const muneco = document.querySelector("#muneco");
+const tituloMensaje = document.querySelector(".titulo-mensaje");
+const encriptado = document.querySelector('.encriptado');
+const mensajeAjustado = document.querySelector(".mensaje-encriptado");
+const mensajeInicial = document.querySelector(".parrafo");
+const botonCopiar = document.querySelector(".btn-copiar");
+
+let reemplazo = [
+    ["e", "enter"],
+    ["o", "ober"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["u", "ufat"]
+]
+
+const resultadoFinal = (nuevoValor) => {
+    mensajeFinal.innerHTML = nuevoValor;
+
+    muneco.style.display = "none";
+    tituloMensaje.style.display = "none";
+    encriptado.classList.add("encriptado-ajustar"); 
+    mensajeAjustado.classList.add("encriptado-ajustar");
+    mensajeFinal.style.display = "block";
+    mensajeInicial.style.display = "none";
+    botonCopiar.style.display = "block";
+}
+
+botonEncriptar.addEventListener("click", () => {
+    const texto = ingresoTexto.value.toLowerCase();
+
+    const encriptar = (nuevoTexto) => {
+        for (let i = 0; i < reemplazo.length; i++) {
+            if (nuevoTexto.includes(reemplazo[i][0])){
+                nuevoTexto = nuevoTexto.replaceAll(reemplazo[i][0],reemplazo[i][1])
+            }
+        }
+        return nuevoTexto;
+
+    }
+
+    resultadoFinal(encriptar(texto));
+    
+
+})
+
+botonDesencriptar.addEventListener("click", () => {
+    const texto = ingresoTexto.value.toLowerCase();
+
+    const desencriptar = (nuevoTexto) => {
+        for (let i = 0; i < reemplazo.length; i++) {
+            if (nuevoTexto.includes(reemplazo[i][1])){
+                nuevoTexto = nuevoTexto.replaceAll(reemplazo[i][1],reemplazo[i][0])
+            }
+        }
+        return nuevoTexto;
+
+    }
+
+    resultadoFinal(desencriptar(texto));
+    
+
+})
+
+botonCopiar.addEventListener("click", () => {
+
+     mensajeFinal.select();
+     document.execCommand("copy");
+     window.getSelection().removeAllRanges();
+
+     Swal.fire({
+        position: "center",
+        icon: "success",
+        title:"Texto copiado al portapapeles",
+        showConfirmButton: false,
+        timer: 1500
+      });
+  
+     
+});
